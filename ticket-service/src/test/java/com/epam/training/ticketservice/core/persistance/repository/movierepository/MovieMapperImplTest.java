8c461050d51070639d079212bc6274a774b15498
Marcell Szegedi <marcszeg27@gmail.com>
2021-05-12 15:43:13 +0200

    tests

---
+++ ticket-service/src/test/java/com/epam/training/ticketservice/core/persistance/repository/movierepository/MovieMapperImplTest.java
@@ -0,0 +1,38 @@
+package com.epam.training.ticketservice.core.persistance.repository.movierepository;
+
+import com.epam.training.ticketservice.core.Movie;
+import com.epam.training.ticketservice.core.persistance.entity.MovieEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieMapperImplTest {
+
+    private MovieMapperImpl movieMapperUnderTest = new MovieMapperImpl();
+
+    private static final Movie MOVIE = new Movie("Movie", "genre", 90);
+    private static final MovieEntity MOVIE_ENTITY = new MovieEntity("Movie", "genre", 90);
+
+/*
+    @Test
+    void testFromMovieEntityToMovieShouldReturnMovie() {
+        //When
+        Movie actual = movieMapperUnderTest.fromMovieEntityToMovie(MOVIE_ENTITY);
+
+        //Then
+        Assertions.assertEquals(MOVIE, actual);
+    }
+
+
+    @Test
+    void testFromMovieToMovieEntityShouldReturnMovieEntity() {
+        //When
+        MovieEntity actual = movieMapperUnderTest.fromMovieToMovieEntity(MOVIE);
+
+        //Then
+        Assertions.assertEquals(MOVIE_ENTITY, actual);
+    }*/
+}
---
+++ ticket-service/src/test/java/com/epam/training/ticketservice/core/persistance/repository/userrepository/UserMapperImplTest.java
@@ -0,0 +1,28 @@
+package com.epam.training.ticketservice.core.persistance.repository.userrepository;
+
+import com.epam.training.ticketservice.core.User;
+import com.epam.training.ticketservice.core.persistance.entity.UserEntity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserMapperImplTest {
+
+    private UserMapperImpl userMapperUnderTest = new UserMapperImpl();
+
+    private static final User USER = new User("user", "password", true);
+    private static final UserEntity USER_ENTITY = new UserEntity("user", "password", true);
+
+/*
+    @Test
+    void testFromUserEntityToUserShouldReturnUser() {
+        //When
+        User actual = userMapperUnderTest.fromUserEntityToUser(USER_ENTITY);
+
+        //Then
+        Assertions.assertEquals(USER, actual);
+    }*/
+
+
+}
