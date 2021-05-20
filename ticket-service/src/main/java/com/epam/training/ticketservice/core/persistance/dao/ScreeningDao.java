2173ed02a64ab3a0a8ad71db462d9eaa94027343
Marcell Szegedi <marcszeg27@gmail.com>
2021-05-20 10:37:04 +0200

    updates

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/persistance/dao/ScreeningDao.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/core/persistance/dao/ScreeningDao.java
@@ -4,13 +4,12 @@ import com.epam.training.ticketservice.core.persistance.entity.ScreeningEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface ScreeningDao extends JpaRepository<ScreeningEntity, UUID> {
-    Optional<ScreeningEntity> findByMovieTitleAndRoomNameAndStartDate(String movie, String room,
+    Optional<ScreeningEntity> findByMovieTitleAndRoomNameAndStartDate(String movieTitle, String roomName,
                                                                       LocalDateTime startDate);
 
-    void deleteByMovieTitleAndRoomNameAndStartDate(String movie, String room, LocalDateTime startDate);
+    void deleteByMovieTitleAndRoomNameAndStartDate(String movieTitle, String roomName, LocalDateTime startDate);
 }
